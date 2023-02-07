fb491bdc40065851a9fd4c348263fa14e1de4bf7
MattGDwyer <matthewgdwyer@gmail.com>
2023-02-07 17:38:11 +0700

    setting up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Row, Column } from 'react-bootstrap';
 
-function App() {
+const App = () => {
+  const [volume, setVolume] = useState(50);
+  const [power, setPower] = useState(true);
+  const [soundSet, setSoundSet] = useState(1);
+
+  const handleVolumeChange = (e) => {
+    setVolume(e.target.value);
+  };
+
+  const handlePowerToggle = () => {
+    setPower(!power);
+  };
+
+  const handleSoundSetToggle = () => {
+    setSoundSet(soundSet === 1 ? 2 : 1);
+  };
+
+  const handleSoundPlay = (sound) => {
+    console.log(`Playing sound: ${sound}`);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Row>
+        <h2>Drum Machine</h2>
+      </Row>
+      <div>
+        <h3>Volume: {volume}</h3>
+        <input
+          type="range"
+          min="0"
+          max="100"
+          value={volume}
+          onChange={handleVolumeChange}
+        />
+      </div>
+      <div>
+        <h3>Power: {power ? 'On' : 'Off'}</h3>
+        <button onClick={handlePowerToggle}>Toggle Power</button>
+      </div>
+      <div>
+        <h3>Sound Set: {soundSet}</h3>
+        <button onClick={handleSoundSetToggle}>Toggle Sound Set</button>
+      </div>
+      <div>
+        <button onClick={() => handleSoundPlay('sound1')}>Sound 1</button>
+        <button onClick={() => handleSoundPlay('sound2')}>Sound 2</button>
+        <button onClick={() => handleSoundPlay('sound3')}>Sound 3</button>
+        <button onClick={() => handleSoundPlay('sound4')}>Sound 4</button>
+        <button onClick={() => handleSoundPlay('sound5')}>Sound 5</button>
+        <button onClick={() => handleSoundPlay('sound6')}>Sound 6</button>
+        <button onClick={() => handleSoundPlay('sound7')}>Sound 7</button>
+        <button onClick={() => handleSoundPlay('sound8')}>Sound 8</button>
+        <button onClick={() => handleSoundPlay('sound9')}>Sound 9</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
